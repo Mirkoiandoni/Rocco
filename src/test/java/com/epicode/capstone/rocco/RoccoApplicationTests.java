c47968ad3baf805cd5baca34722446d65da34ee2
Mirko iandoni <mirko.96@live.it>
2023-09-05 17:33:31 +0200

    modifico

---
+++ src/test/java/com/epicode/capstone/rocco/RoccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.capstone.rocco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
